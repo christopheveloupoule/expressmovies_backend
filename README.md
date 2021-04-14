044e46becc6962c8c0e3104539dd10f8dd9aec2e
Christophe <christophevelou@gmail.com>
2021-04-14 20:27:47 +0200

    Utilisation template ejs

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
+Résumé:
+
+Créer une application web à l'aide de Node.js et Express.js et la sécuriser à l’aide de JWT (JSON Web Token). Notamment créer plusieurs pages, naviguer d'une page à l'autre, créer des formulaires et poster des données, effectuer des requêtes vers des API REST, et à faire du CRUD vers une base MongoDB à l'aide de Mongoose.
+
 I Routage en détail
 * Ajout de paramètre à une route | Importance de l'ordre des routes
+
+II View Engines (moteurs de vues)
+
+* Créer un template à l'aide de EJS (module) : npm install ejs --save
+Pr utiliser un template dans l'app on utilise la méthode "render"
+Pour utiliser le fichier style.css, on aura recours à un middleware (app.use) car le css est un fichier statique
+
+* Pratiquer l'utilisation de template:
+Par exemple on crée, movie detail.ejs ...
+
+* Passer des données de l'appli JS à une vue (movie-details):
+On passe un 2eme param à la méthode render de app.get('/movies/:id'...
+
 
--- app.js
+++ app.js
@@ -3,21 +3,36 @@ const app = express();
 
 const PORT = 3001;
 
+//Chemin pour y accéder au fichier static (css)
+app.use('/public' , express.static('public'))
+
+//Conf à faire pr utiliser notre template "engine"
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 app.get('/movies', (req,res) => {
-    res.send('Bientot des films ici même !')
+    //res.send('Bientot des films ici même !')
+    res.render('movies');
 })
 
+// app.get('/movie-details', (req,res) => {
+//     res.render('movie-details');
+// });
+
 app.get('/movies/add', (req,res) => {
-    res.send(`Bien*tot un formulaire d'ajout ici`)
+    res.send(`Bien*tot un formulaire d'ajout ici`);
 })
 
 app.get('/movies/:id', (req,res) => {
     const id = req.params.id;
-    res.send(`Voici le film numéro ${id}`)
+    const title = req.params.title;
+    // res.send(`Voici le film numéro ${id}`)
+    res.render('movie-details', {movieid: id });
 })
 
 app.get('/', (req,res) => {
-    res.send('hello world!!!!')
+    //res.send('hello world!!!!');
+    res.render('index');
 });
 
 app.listen(PORT, () => {
