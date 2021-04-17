fed0f69d7a0be2e7b933f8eaedaeb61f71bd2234
Christophe <christophevelou@gmail.com>
2021-04-17 13:18:09 +0200

    formulaire

--- README.md
+++ README.md
@@ -3,9 +3,12 @@ Résumé:
 Créer une application web à l'aide de Node.js et Express.js et la sécuriser à l’aide de JWT (JSON Web Token). Notamment créer plusieurs pages, naviguer d'une page à l'autre, créer des formulaires et poster des données, effectuer des requêtes vers des API REST, et à faire du CRUD vers une base MongoDB à l'aide de Mongoose.
 
 I Routage en détail
+**************************************************************************
 * Ajout de paramètre à une route | Importance de l'ordre des routes
 
+
 II View Engines (moteurs de vues)
+***************************************************************************
 
 * Créer un template à l'aide de EJS (module) : npm install ejs --save
 Pr utiliser un template dans l'app on utilise la méthode "render"
@@ -20,5 +23,41 @@ On passe un 2eme param à la méthode render de app.get('/movies/:id'...
 * Gestion des partials (footer header)
 
 * Gestion de l'envoi de fichier statique (on applique simplement une classe CSS separation avec le footer)
+
+* Passer des données complexes à une vue et itérer côté template
+ex: via un tableau d'objet. Il faut passer le [] au template via le res.render(Nom template,les données )
+On pourra passer également un titre via la const title
+On va itérer sur un []
+
+III.FORMULAIRES
+***************************************************************************
+
+* Créer un formulaire et gérer le POST: 
+Installer le middleware charger de récupérer le body d'une requete via "Bodyparser"
+
+npm install --save body-parser
+
+Pour l'instant body-parser s'applique à toute les requêtes.
+Pour l'appliquer à une route spécifique, on passe le "urlencoded" en 2eme paramètre de la route app.post et on passe en comment:
+"app.use(bodyParser.urlencoded({ extended: false }))" en haut de liste
+
+* Récupérer le body d'une requête  via n middleware body-parser:
+On recupe granuleremet via le req.body
+
+* Gerer la persistance en mémoire: 
+On va déclarer la méthode "frenchMovies en dehors de la méthode app.get
+
+* Poster un formulaire Ajax : Pour ne pas que la page soit recharger en postant le form
+Avec le fetch, on va faire du POST
+On va utiliser "multer", un middleware pr recupérer les données.
+
+* Poster un formulaire via le middleware "multer":
+npm install --save multer, pour POST les le form via les photos par ex...
+
+
+
+
+
+
 
 
--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer();
 
-const PORT = 3002;
+
+const PORT = 3000;
+let frenchMovies = [];
 
 //Chemin pour y accéder au fichier static (css)
 app.use('/public' , express.static('public'))
+
+//Fonction : récuperer du body et son contenu (les champs saisis ds le form)
+//app.use(bodyParser.urlencoded({ extended: false }))
 
 //Conf à faire pr utiliser notre template "engine"
 app.set('views', './views');
@@ -12,15 +20,51 @@ app.set('view engine', 'ejs');
 
 app.get('/movies', (req,res) => {
     //res.send('Bientot des films ici même !')
-    res.render('movies');
+
+    const title = 'Les meilleurs films français des 30 dernières années'
+
+    frenchMovies = [
+        { title: ' La grande vadrouille ', year: 1978},
+        { title: ' Intouchable ', year: 2014},
+        { title: ' Le diner de cons', year: 1998},
+        { title: ' Leon', year: 2000}
+    ]
+    res.render('movies', {movies: frenchMovies, title: title});
 })
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+/*app.post('/movies', urlencodedParser, (req,res) => {
+    console.log('le titre: ',req.body.movietitle);
+    console.log('année: ',req.body.movieyear);
+    const newMovie = { title: req.body.movietitle, year: req.body.movieyear };
+   // frenchMovies.push(newMovie); on modifie un tableau existant
+   frenchMovies = [...frenchMovies, newMovie]; // on crée un nouveau []
+   console.log(frenchMovies);
+
+    res.sendStatus(201);
+})*/
+
+app.post('/movies', upload.fields([]), (req,res) => {
+    if(!req.body) {
+        return res.sendStatus(500);
+    }else {
+        const formData = req.body;
+        console.log('formData: ', formData);
+        const newMovie = { title: req.body.movietitle, year: req.body.movieyear };
+        // frenchMovies.push(newMovie); on modifie un tableau existant
+        frenchMovies = [...frenchMovies, newMovie]; // on crée un nouveau []
+        res.sendStatus(201);
+    }
+})
+
 
 // app.get('/movie-details', (req,res) => {
 //     res.render('movie-details');
 // });
 
 app.get('/movies/add', (req,res) => {
-    res.send(`Bien*tot un formulaire d'ajout ici`);
+    res.send(`Bientot un formulaire d'ajout ici`);
 })
 
 app.get('/movies/:id', (req,res) => {
