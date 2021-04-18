b146379db3e03817c63b171a6509b98ae90ff28b
Christophe <christophevelou@gmail.com>
2021-04-18 12:37:57 +0200

    JWT part1

--- README.md
+++ README.md
@@ -54,6 +54,51 @@ On va utiliser "multer", un middleware pr recupérer les données.
 * Poster un formulaire via le middleware "multer":
 npm install --save multer, pour POST les le form via les photos par ex...
 
+IV.API: Requeter une API REST grandeur nature
+****************************************************************
+
+* Presentation API de 'The Movie DB':
+51337e213f1f1eb7dd3d7150e177a88a
+
+* Faire une requete à l'aide de axios:
+Ajout movie-search.ejs et le link sur le header.ejs
+Utilisation d'un CDN pour Axios
+
+* Parser les résultats et les intégrer ds une page web
+
+V.JSON WEB TOKEN (JWT) - 1ere partie:
+
+* Création d'une page login.ejs et gestion du GET et du POST dans le app.js
+
+* Générer un TOKEN et l'envoyer en réponse à un client:
+npm install --save jsonwebtoken
+Method 'sign' dans la doc
+
+* Sauvegarder notre token dans localStorage
+
+* Modifier la vue en fonction de la présence d'un TOKEN:
+function switchTLoggedinMode | function switchToLoggedOutMode | function disconnect
+
+* Ajouter un lienvers la page de connexion:
+/login dans le header.js
+
+* Détecter le moment ou le DOM est prêt:
+Modife du footer.ejs, on invoque immédiatement un function expression
+
+* Gérer l'affichage du formulaire en fonction de la présence d'un token:
+var connectionLinkArea = document.querySelector ('#connectionLink');
+
+VI.JSON WEB TOKEN (JWT) - 2eme partie:
+
+
+
+
+
+
+
+
+
+
 
 
 
--- app.js
+++ app.js
@@ -4,19 +4,25 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const upload = multer();
 
+const jwt = require('jsonwebtoken')
 
-const PORT = 3000;
+
+const PORT = 3003;
 let frenchMovies = [];
-
-//Chemin pour y accéder au fichier static (css)
-app.use('/public' , express.static('public'))
-
-//Fonction : récuperer du body et son contenu (les champs saisis ds le form)
-//app.use(bodyParser.urlencoded({ extended: false }))
 
 //Conf à faire pr utiliser notre template "engine"
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
+//Chemin pour y accéder au fichier static (css)
+app.use('/public' , express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+//Fonction : récuperer du body et son contenu (les champs saisis ds le form)
+//app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/movies', (req,res) => {
     //res.send('Bientot des films ici même !')
@@ -30,8 +36,10 @@ app.get('/movies', (req,res) => {
         { title: ' Leon', year: 2000}
     ]
     res.render('movies', {movies: frenchMovies, title: title});
-})
+});
 
+// create application/x-www-form-urlencoded parser
+// https://github.com/expressjs/body-parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 /*app.post('/movies', urlencodedParser, (req,res) => {
@@ -45,18 +53,19 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
     res.sendStatus(201);
 })*/
 
+//!\ In upload.fields([]), the empty array '[]' is required
 app.post('/movies', upload.fields([]), (req,res) => {
     if(!req.body) {
         return res.sendStatus(500);
-    }else {
+    } else {
         const formData = req.body;
         console.log('formData: ', formData);
-        const newMovie = { title: req.body.movietitle, year: req.body.movieyear };
+        const newMovie = { title: formData.movietitle, year: formData.movieyear };
         // frenchMovies.push(newMovie); on modifie un tableau existant
         frenchMovies = [...frenchMovies, newMovie]; // on crée un nouveau []
         res.sendStatus(201);
     }
-})
+});
 
 
 // app.get('/movie-details', (req,res) => {
@@ -65,19 +74,48 @@ app.post('/movies', upload.fields([]), (req,res) => {
 
 app.get('/movies/add', (req,res) => {
     res.send(`Bientot un formulaire d'ajout ici`);
-})
+});
 
 app.get('/movies/:id', (req,res) => {
     const id = req.params.id;
     const title = req.params.title;
     // res.send(`Voici le film numéro ${id}`)
     res.render('movie-details', {movieid: id });
-})
-
-app.get('/', (req,res) => {
-    //res.send('hello world!!!!');
-    res.render('index');
 });
+
+app.get('/movie-search', (req, res) => {
+    res.render('movie-search');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', { title: 'Espace membre'});
+});
+
+const fakeUser = { email: 'testuser@testmail.fr', password: '123456789' };
+const secret = 'qsdjS12ozehdoIJ123DJOZJLDSCqsdeffdg123ER56SDFZedhWXojqshduzaohduihqsDAqsdq';
+
+app.post('/login', urlencodedParser, (req, res) => {
+    console.log('login post', req.body);
+    if (!req.body) {
+        return res.sendStatus(500);
+    } else {        
+        if(fakeUser.email === req.body.email && fakeUser.password === req.body.password) {
+            /*res.json({ 
+                        email: 'testuser@testmail.fr', 
+                        favoriteMovie: 'PULP FICTION',
+                        favoriteMovieTheater: 'Place Gaumont, 31000 Toulouse', 
+                        lastLoginDate: new Date() 
+                    });*/
+            // iss means 'issuer'
+            const myToken = jwt.sign({iss: 'http://expressmovies.fr', user: 'christophevelou', role: 'moderator'}, secret);
+            console.log('myToken', myToken);
+            res.json(myToken);
+        } else {
+            res.sendStatus(401);
+        } 
+    } 
+});
+
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
